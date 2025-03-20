f0a704ae3eaf0be19dfe2790b82c12126c4f0cf6
Jorge Leites <jorge@eagerworks.com>
2025-03-20 13:28:14 -0300

    Add specs for home controller

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Home", type: :request do
+  describe "GET /index" do
+    subject { get root_path }
+
+    it 'renders the index template' do
+      subject
+      expect(response).to render_template(:index)
+    end
+
+    it 'returns a successful response' do
+      subject
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
