34ae0bcd68c7b3e765acd00c5241e7e7342223a2
Jorge Leites <jorge@eagerworks.com>
2025-06-11 16:01:57 -0300

    Move user data to terraform repo

--- scripts/user_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Update and install necessary packages
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y docker.io curl git
-
-# Add the user to the docker group
-sudo usermod -aG docker ubuntu
